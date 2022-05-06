8ab5a666ef2401a6f5390b50152f12eaa39d4eb1
DuongHao999 <duongtuhao999@email.com>
2022-05-06 14:40:48 +0700

    update model

--- src/main/java/com/example/relationship/model/Cart.java
+++ src/main/java/com/example/relationship/model/Cart.java
@@ -1,4 +1,46 @@
 package com.example.relationship.model;
 
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "cart")
+
 public class Cart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String name;
+
+    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Item> items = new ArrayList<>();
+
+    public Cart() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Item> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Item> items) {
+        this.items = items;
+    }
 }
--- src/main/java/com/example/relationship/model/Item.java
+++ src/main/java/com/example/relationship/model/Item.java
@@ -1,4 +1,48 @@
 package com.example.relationship.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "item")
 public class Item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String serialNumber;
+
+    @ManyToOne
+    @JoinColumn(name = "card_id")
+    private Cart cart;
+
+    public Item() {
+    }
+
+    public Item(String serialNumber, Cart cart) {
+        this.serialNumber = serialNumber;
+        this.cart = cart;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public Cart getCart() {
+        return cart;
+    }
+
+    public void setCart(Cart cart) {
+        this.cart = cart;
+    }
 }
